8a633b3c7b27a7d95d8fadabe76e772d9cdc72fc
NaomiNyakaru <naomi.nyakaru@gmail.com>
2024-10-12 21:46:13 +0300

    validation

--- server/models.py
+++ server/models.py
@@ -12,6 +12,24 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def name_validation(self,key,name_authors):
+        if not name_authors:
+            raise ValueError("name required")
+        existing_author = Author.query.filter_by(name=name_authors).first()
+        if existing_author:
+            raise ValueError("Name already exists")
+        return name_authors
+     
+
+    @validates('phone_number')
+    def phone_validation(self,key,phone_authors):
+        if len(phone_authors) != 10:
+            raise ValueError("Incorrect phone format")
+        if not phone_authors.isdigit():
+            raise ValueError("Phone Number must contains only digits")
+        return phone_authors
+
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +45,32 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators
+    @validates('title')
+    def title_validate(self,key,post_title):
+        if not post_title:
+            raise ValueError("Post must have a title")
+        return post_title
+    
+    @validates('content')
+    def content_validate(self,key,post_content):
+        if len(post_content) < 250:
+            raise ValueError("Content too short")
+        return post_content
+
+    @validates('summary')
+    def summary_validate(self,key,post_summary):
+        if len(post_summary) > 250:
+            raise ValueError("Summary too long")
+        return post_summary
+
+    @validates('category')
+    def category_validate(self, key, post_category):
+        allowed_categories = ['Health', 'Non-Fiction', 'Entertainment']
+        if post_category not in allowed_categories:
+            raise ValueError("Category is not valid")
+        return post_category
+
 
 
     def __repr__(self):
